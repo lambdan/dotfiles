2287b21b555ab4e3d43b990cbad8f203dfaeadb8
djs <lambdan@icloud.com>
2025-12-22 09:20:58 +0100

    copilot toggle

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---
+
+-- Copilot suggestions dont work without this?
 vim.g.ai_cmp = false
 
 vim.g.root_spec = { "lsp", { ".git", "lua", "build.sbt" }, "cwd" }
